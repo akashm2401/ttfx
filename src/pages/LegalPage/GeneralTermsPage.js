ee83c18823f7f2f576b37f3c1c2851b5ff5853fb
Akash More <akashkmore2002@gmail.com>
2024-06-12 01:29:43 +0530

    worked on legal pages

---
+++ src/pages/LegalPage/GeneralTermsPage.js
@@ -0,0 +1,128 @@
+import React from 'react';
+import './LegalCommon.css'
+
+export default function GeneralTermsPage() {
+  return (
+    // <div>GeneralTermsPage</div>
+    <div className="legal-info-container">
+      <div className="heading">
+        <h3>General Buisness Terms</h3>
+      </div>
+
+      <div className="heading">
+        <h4>Risk Notice</h4>
+      </div>
+      <br />
+      <div className="text">
+        <p>We provide services for trading derivative financial contracts. Our contracts are traded on a margin or leveraged basis, a type of trading which carries a high degree of risk to your capital. The price of the contract you make with us may change quickly and your profits and losses may be more than the amount of your investment or funds. If you do not have sufficient funds to meet your margin requirements, then we may close your open positions immediately and without notice. Please read the Risk Warning Notice carefully to understand the risks of trading on a margin or leverage basis. You should not deal in our contracts unless you understand and accept the risks of margin trading. Trading in these products may not be suitable for everyone.</p>
+      </div>
+      <br />
+
+      <div className="heading">
+        <h4>The Scope of This Aggreement</h4>
+      </div>
+      <br />
+      <div className="heading">
+        <h5>1. Introduction </h5>
+      </div>
+      <br />
+      <div className="text">
+        <p>1.1	These General Terms are part of the agreement between True Trade Pro LTD (TTP) and (“you” or “client”) which governs our trading services and all transactions we conduct with you. </p>
+        <br />
+        <p>1.2	We are authorized and regulated in Malaysia by the Labuan Financial Conduct Authority (“FSA) registration number MB/22/0103, to carry on money broking business pursuant to Sections 90 and 92 of Labuan Financial Services and Securities Act 2010 (LFSSA) or Sections 65 and 67 of Labuan Islamic Financial Services and Securities Act 2010 (LIFSSA). Our registered address (Company No: LL18002) is Lot A Level 1 Lazenda Commercial Centre Phase 3, Jalan OKK Abullah, Labuan F.T. Malaysia 87000.  </p>
+        <br />
+        <p>1.3	The agreement between us relating to our trading services consists of the following documents:
+          <ul>
+            <li>Application Form.</li>
+            <li>These General Terms and</li>
+            <li>The Supplemental Terms for the relevant Product.</li>
+          </ul>
+
+          Together these documents are referred to as the “Agreement”.
+        </p>
+        <br />
+        <p>1.4 The Agreement supersedes all our previous terms and conditions and any    amendments thereto and will be effective from the specified date or the date we acknowledge acceptance of your Application Form.</p>
+        <br />
+        <p>1.5	Each Product we offer is subject to its Supplemental Terms. The Supplemental Terms provide you with information on the type and nature of the Products in relation to which we offer services under this Agreement, and they set out the terms and conditions under which these Products are offered. Subject to clause 4.19, should there be any conflict between these General Terms and the Supplemental Terms, the Supplemental Terms will prevail. </p>
+        <br />
+        <p>1.6	We provide guidance on, and warnings of, the risks associated with the Products made available under this Agreement in Annex 1 to these General Terms.</p>
+        <br />
+        <p>1.7	Other materials which explain the basis upon which we trade with you but are not part of the Agreement include:
+          <ul>
+            <li>
+              The Market Information, which provides the commercial details for each Market, including Market Hours, Margin Factors, and other requirements for dealing in each Market. Market Information is located on the Trading Platform. In the event you elect to use a third-party hosting or trading application (for example, MetaTrader), information specific to such third-party hosting or trading application located on the Website shall supplement the Market Information; however, to the extent there are any inconsistencies the Market Information will prevail. We may amend the Market Information from time to time and will make current versions of the Market Information available to you on the Trading Platform.
+            </li>
+            <li>
+              Our website – including our Trading Platform via which you will trade with us; and
+            </li>
+            <li>
+              Our notices and policies – the Risk Warning Notice, our Trade and Order Execution Policy, our Conflicts of Interest Policy, and any notices with respect to trading tools, third-party trading platforms and Arbitration Jurisdictions (together “Notices and Policies”). These are located in the Annexes to the General Terms. We may amend our Notices and Policies from time to time and will make current versions of our Notices and Policies available to you on our website and/or in the Annexes to the General Terms.
+            </li>
+          </ul>
+        </p>
+        <br />
+        <p>1.8	Please read the Agreement and the Notices and Policies carefully and discuss with us  anything which you do not understand. Unless we have agreed in writing that any part of this Agreement is not to apply, we will regard this Agreement as setting out all the relevant terms concerning our trading services and any Trades which you enter into with us. Trades that we enter into with you under this Agreement are legally binding and enforceable. By signing the Application Form or by electronically submitting your application on our website or, if applicable, via a mobile application, you confirm that you accept the terms of the Agreement. When we open an Account for you, you will be bound by the Agreement in your dealings with us.</p>
+        <br />
+        <p>1.9	Unless written notice is required in accordance with this Agreement, you may      communicate with us in writing (including by email or other electronic means) or orally  (including by telephone). Email, chat, text, instant messaging features whether transmitted through the internet, a proprietary network, a computer, a pager, or another wireless device or otherwise may be provided to you as a convenience to enhance your communications with us. Except where otherwise provided in clause 4.1 you shall not use these features to request, authorize or affect any transaction, to send fund transfer instructions or for any other communication that requires non-electronic written authorization. We shall not be responsible for any loss or damage that results if any request is not accepted or processed. You agree that you shall use these features in compliance with applicable laws and regulations, and you shall not use them to transmit inappropriate information, including information that may be deemed obscene, defamatory, harassing, or fraudulent.</p>
+        <br />
+        <p>1.10 The language of communication shall be English, and you will receive documents and other information from us in English. By opening an Account with us, you agree to receive trading services from us in English and subject to the English terms and conditions of this Agreement. We may in our sole discretion provide local language support. If a document is translated into another language this will be for information purposes only and the English version will prevail.</p>
+      </div>
+
+      <br />
+      <div className="heading">
+        <h4>Dealing With Us</h4>
+      </div>
+
+      <br />
+
+      <div className="heading">
+        <h5>2.	General Information </h5>
+      </div>
+
+      <br />
+
+      <div className="text">
+        <p>
+          2.1	Our trading service is an electronic service, and you specifically consent to us communicating with you by email, by SMS, by letter or telephone and/or by any other electronic means or methods agreed in writing. We will not send a paper form of any communication sent to you unless you request us to do so. We reserve the right to charge for communications and/or documents sent to you in a paper form.
+        </p>
+        <br />
+        <p>
+
+          2.2	You confirm that you have regular access to the internet and consent to us providing you with information about us and our services (including the Market Information), our costs and charges, information about our Products, our Notices and Policies and any other information that we are required to provide to you pursuant to LFSA Rules by email or by posting such information on our website or the Trading Platform. This is without prejudice to any rights you may have to request copies of the information or, by notifying us in writing, to receive such information in hard copy, rather than by email or other electronic means. We reserve the right to charge for information and/or documents sent to you in a paper form.
+        </p>
+        <br />
+        <p>
+
+          2.3	Unless we notify you otherwise, we will classify you as a Retail Client for the purpose of LFSA Rules. You have a right to request a different categorization but if we agree to this request, you will lose the protection of certain LFSA Rules. In certain circumstances we may wish to re-categories you, but if we do so we will explain clearly why we are doing this and the effect this will have on your protections and compensation rights.
+        </p>
+        <br />
+        <p>
+
+          2.4	We will deal with you as principal and not as agent on your behalf. This means that any Trades are agreed directly between you and us and we will be the counterparty to all of your Trades.
+        </p>
+        <br />
+        <p>
+
+          2.5	Unless we agree otherwise in writing, you will also deal with us as principal and not as an agent or representative of another person. You will not permit any person to deal on your behalf unless we agree that such person (the “Agent”) can act on your behalf. We will be entitled to rely on any instructions given to us by the Agent in relation to your Account. We may require confirmation that the Agent has authority to act on your behalf at any time we reasonably consider appropriate
+        </p>
+        <br />
+        <p>
+
+          2.6	In order to ensure that we can deal with you and discharge our obligations under applicable laws and regulations, you will provide us, as appropriate, with your legal entity identifier (“LEI”) (provided you are eligible for one) or such other information relating to, for example, your nationality as we will require to determine your national client identifier.
+        </p>
+        <br />
+        <p>
+
+          2.7	We shall not give advice to you on the merits of any Trade and shall deal with you on an execution-only basis. None of our staff are authorized by us or permitted under LFSA Rules to give you investment advice. Accordingly, you should not regard any proposed trades, suggested trading strategies or other written or oral communications from us as investment recommendations or advice or as expressing our view as to whether a particular trade is suitable for you or meets your financial objectives. You must rely on your own judgement for any investment decision you make in relation to your Account. If you require investment or tax advice, please contact an independent investment or tax adviser.
+        </p>
+        <br />
+        <p>
+
+          2.8	You will not have any rights of ownership, delivery or otherwise in any Underlying Instrument as a result of a Trade with us. We will not transfer any Underlying Instrument or any rights (such as voting rights or delivery obligations) in it to you.
+        </p>
+
+      </div>
+
+    </div>
+  )
+}
