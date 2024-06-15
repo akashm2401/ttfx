e483964ec31019172908ac88ec411507180e5ec2
Akash More <akashkmore2002@gmail.com>
2024-06-15 12:59:24 +0530

    scroll top on page load

--- src/pages/AboutPage/AboutPage.js
+++ src/pages/AboutPage/AboutPage.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './AboutPage.css'
 
 export default function AboutPage() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+  
   return (
     <>
       <div className="about-page">
