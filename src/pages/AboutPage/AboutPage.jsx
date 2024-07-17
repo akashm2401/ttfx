c8bc8d4b936dfdce7a4bbdef8bba756214e5837f
Akash More <akashkmore2002@gmail.com>
2024-07-18 00:28:28 +0530

    About Json Integration done

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -1,10 +1,24 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./AboutPage.scss";
+import data from './AboutPageData.json';
+import SpinnerComponent from '../../components/SpinnerComponent/SpinnerComponent';
 
 export default function AboutPage() {
+  const [aboutData, setAboutData] = useState(null);
+
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
+  
+  useEffect(() => {
+    // Simulate fetching data
+    setAboutData(data);
+  }, []);
+
+  if (!aboutData) {
+    return <div><SpinnerComponent /></div>;
+  }
+  
 
   return (
     <div className="about-comp">
@@ -12,18 +26,10 @@ export default function AboutPage() {
         <div className="row m-0 about-page-img-container">
           <div className="col-0 col-lg-6"></div>
           <div className="col-12 col-lg-6 about-page-img-container-info pe-5">
-            <h2>Lorem ipsum dolor sit amet consectetur.</h2>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
-              magnam autem aliquam expedita fugit nemo, cumque illum tenetur
-              necessitatibus repudiandae modi voluptatem cupiditate similique.
-              Similique sequi odio vero velit repudiandae!
-            </p>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas
-              nihil perspiciatis consequuntur cupiditate architecto dolor,
-              labore eveniet saepe pariatur vel! Doloribus, est!
-            </p>
+            <h2>{aboutData.aboutPageImgContainerInfo.title}</h2>
+            {aboutData.aboutPageImgContainerInfo.paragraphs.map((para, index) => (
+              <p key={index}>{para}</p>
+            ))}
           </div>
         </div>
 
@@ -31,55 +37,15 @@ export default function AboutPage() {
           <div className="container">
             <div className="row justify-content-between p-0 m-0">
               <div className="col-12 col-lg-4 about-info-heading">
-                <h4>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                </h4>
+                <h4>{aboutData.aboutInfoContainer.heading}</h4>
               </div>
               <div className="col-12 col-lg-8 row p-0 m-0 about-info-content">
-                <div className="col-12 col-md-6 mb-4">
-                  <p className="about-info-content-heading">
-                    Navigate the global markets
-                  </p>
-                  <p>
-                    Everything you need to navigate the global markets is at
-                    your fingertips in the Saxo platforms. Choose from a wide
-                    choice of financial products and hundreds of different ways
-                    to trade and invest.
-                  </p>
-                </div>
-                <div className="col-12 col-md-6 mb-4">
-                  <p className="about-info-content-heading">
-                    Navigate the global markets
-                  </p>
-                  <p>
-                    Everything you need to navigate the global markets is at
-                    your fingertips in the Saxo platforms. Choose from a wide
-                    choice of financial products and hundreds of different ways
-                    to trade and invest.
-                  </p>
-                </div>
-                <div className="col-12 col-md-6 mb-4">
-                  <p className="about-info-content-heading">
-                    Navigate the global markets
-                  </p>
-                  <p>
-                    Everything you need to navigate the global markets is at
-                    your fingertips in the Saxo platforms. Choose from a wide
-                    choice of financial products and hundreds of different ways
-                    to trade and invest.
-                  </p>
-                </div>
-                <div className="col-12 col-md-6 mb-4">
-                  <p className="about-info-content-heading">
-                    Navigate the global markets
-                  </p>
-                  <p>
-                    Everything you need to navigate the global markets is at
-                    your fingertips in the Saxo platforms. Choose from a wide
-                    choice of financial products and hundreds of different ways
-                    to trade and invest.
-                  </p>
-                </div>
+                {aboutData.aboutInfoContainer.contents.map((content, index) => (
+                  <div key={index} className="col-12 col-md-6 mb-4">
+                    <p className="about-info-content-heading">{content.heading}</p>
+                    <p>{content.text}</p>
+                  </div>
+                ))}
               </div>
             </div>
           </div>
@@ -88,22 +54,12 @@ export default function AboutPage() {
         <div className="about-insights-container py-5">
           <div className="container px-5">
             <div className="row about-insights-data justify-content-evenly">
-              <div className="col-12 col-sm-6 col-md-4 col-lg-3 mb-2 about-insights-data-item">
-                <p className='fs-9'>1,200,000</p>
-                <p>Clients worldwide</p>
-              </div>
-              <div className="col-12 col-sm-6 col-md-4 col-lg-3 mb-2 about-insights-data-item">
-                <p className='fs-9'>100+ bn</p>
-                <p>USD client assets</p>
-              </div>
-              <div className="col-12 col-sm-6 col-md-4 col-lg-3 mb-2 about-insights-data-item">
-                <p className='fs-9'>260,000</p>
-                <p>Daily executed trades</p>
-              </div>
-              <div className="col-12 col-sm-6 mb-2 col-md-4 col-lg-3 about-insights-data-item">
-                <p className='fs-9'>30+ years</p>
-                <p>Experience in the industry</p>
-              </div>
+              {aboutData.aboutInsightsContainer.map((item, index) => (
+                <div key={index} className="col-12 col-sm-6 col-md-4 col-lg-3 mb-2 about-insights-data-item">
+                  <p className='fs-9'>{item.number}</p>
+                  <p>{item.text}</p>
+                </div>
+              ))}
             </div>
           </div>
         </div>
@@ -113,165 +69,56 @@ export default function AboutPage() {
             <div className="d-flex flex-column align-items-center">
               <h4 className="text-center">Working together to get ahead</h4>
               <p style={{ width: "60%" }} className="text-center">
-                True Trade Pro is powered by the passion, expertise and
-                dedication of hundreds of team members around the world.
+                True Trade Pro is powered by the passion, expertise and dedication of hundreds of team members around the world.
               </p>
             </div>
 
             <div className="row my-4">
-              <div className="col-12 col-lg-4 mb-5">
-                <div className="about-member-info-box p-5 text-center">
-                  <p className="about-member-name">Michael Frey</p>
-                  <p className="about-member-info">
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                    Nemo praesentium optio sequi et ea accusamus aliquid
-                    cupiditate neque.
-                  </p>
-                  {/* <p className="about-member-socials">
-                    Lorem ipsum dolor sit.
-                  </p> */}
-                  <div className="d-flex px-2 my-2 justify-content-around">
-                    <i>
-                      <img
-                        src={require("../../assets/ico/linkedin.png")}
-                        alt=""
-                      />
-                    </i>
-                    <i>
-                      <img
-                        src={require("../../assets/ico/facebook.png")}
-                        alt=""
-                        height={32}
-                      />
-                    </i>
-                    <i>
-                      <img
-                        src={require("../../assets/ico/twitter.png")}
-                        alt=""
-                      />
-                    </i>
+              {aboutData.aboutMembers.map((member, index) => (
+                <div key={index} className="col-12 col-lg-4 mb-5">
+                  <div className="about-member-info-box p-5 text-center">
+                    <p className="about-member-name">{member.name}</p>
+                    <p className="about-member-info">{member.info}</p>
+                    <div className="d-flex px-2 my-2 justify-content-around">
+                      <i>
+                        <img src={require("../../assets/ico/linkedin.png")} alt="" />
+                      </i>
+                      <i>
+                        <img src={require("../../assets/ico/facebook.png")} alt="" height={32} />
+                      </i>
+                      <i>
+                        <img src={require("../../assets/ico/twitter.png")} alt="" />
+                      </i>
+                    </div>
                   </div>
                 </div>
-              </div>
+              ))}
+            </div>
 
-              <div className="col-12 col-lg-4 mb-5">
-                <div className="about-member-info-box p-5 text-center">
-                  <p className="about-member-name">Michael Frey</p>
-                  <p className="about-member-info">
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                    Nemo praesentium optio sequi et ea accusamus aliquid
-                    cupiditate neque.
-                  </p>
-                  {/* <p className="about-member-socials">
-                    Lorem ipsum dolor sit.
-                  </p> */}
-                  <div className="d-flex px-5 my-4 justify-content-around">
-                    <i>
-                      <img
-                        src={require("../../assets/ico/linkedin.png")}
-                        alt=""
-                      />
-                    </i>
-                    <i>
-                      <img
-                        src={require("../../assets/ico/facebook.png")}
-                        alt=""
-                        height={32}
-                      />
-                    </i>
-                    <i>
-                      <img
-                        src={require("../../assets/ico/twitter.png")}
-                        alt=""
-                      />
-                    </i>
-                  </div>
-                </div>
+            <div className="about-more-info row justify-content-center my-5">
+              <div className="col-12 text-center my-4">
+                <h6>{aboutData.aboutMoreInfo[0]}</h6>
+                <h6>{aboutData.aboutMoreInfo[1]}</h6>
               </div>
+              <p style={{ width: "60%" }} className="text-center">{aboutData.aboutMoreInfo[2]}</p>
+              <p style={{ width: "60%" }} className="text-center">{aboutData.aboutMoreInfo[3]}</p>
+            </div>
 
-              <div className="col-12 col-lg-4 mb-5">
-                <div className="about-member-info-box p-5 text-center">
-                  <p className="about-member-name">Michael Frey</p>
-                  <p className="about-member-info">
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                    Nemo praesentium optio sequi et ea accusamus aliquid
-                    cupiditate neque.
-                  </p>
-                  {/* <p className="about-member-socials">
-                    Lorem ipsum dolor sit.
-                  </p> */}
-                  <div className="d-flex px-5 my-4 justify-content-around">
-                    <i>
-                      <img
-                        src={require("../../assets/ico/linkedin.png")}
-                        alt=""
-                      />
-                    </i>
-                    <i>
-                      <img
-                        src={require("../../assets/ico/facebook.png")}
-                        alt=""
-                        height={32}
-                      />
-                    </i>
-                    <i>
-                      <img
-                        src={require("../../assets/ico/twitter.png")}
-                        alt=""
-                      />
-                    </i>
-                  </div>
-                </div>
-              </div>
-
-              <div className="about-more-info row justify-content-center my-5">
-                <div className="col-12 text-center my-4">
-                  <h6>The markets never stand still.</h6>
-                  <h6>And neither do we</h6>
-                </div>
-                <p style={{ width: "60%" }} className="text-center">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Architecto, necessitatibus eum. In nisi cumque expedita? Qui,
-                  similique enim nulla et aperiam vitae repudiandae molestias.
-                  Vel excepturi eos culpa temporibus ut.
-                </p>
-                <p style={{ width: "60%" }} className="text-center">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Cupiditate mollitia quasi cum et odit ex amet voluptatibus
-                  eum, deleniti tenetur.
-                </p>
-              </div>
-
-              <div className="about-social-links text-center my-3">
-                <p>Follow us to get the latest news</p>
-                <div
-                  className="d-flex px-5 my-4 justify-content-around"
-                  style={{ width: "40%", margin: "auto" }}
-                >
-                  <i>
-                    <img
-                      src={require("../../assets/ico/linkedin.png")}
-                      alt=""
-                    />
-                  </i>
-                  <i>
-                    <img
-                      src={require("../../assets/ico/facebook.png")}
-                      alt=""
-                      height={32}
-                    />
-                  </i>
-                  <i>
-                    <img src={require("../../assets/ico/twitter.png")} alt="" />
-                  </i>
-                  <i>
-                    <img
-                      src={require("../../assets/ico/instagram.png")}
-                      alt=""
-                      height={32}
-                    />
-                  </i>
-                </div>
+            <div className="about-social-links text-center my-3">
+              <p>Follow us to get the latest news</p>
+              <div className="d-flex px-5 my-4 justify-content-around" style={{ width: "40%", margin: "auto" }}>
+                <i>
+                  <img src={require("../../assets/ico/linkedin.png")} alt="" />
+                </i>
+                <i>
+                  <img src={require("../../assets/ico/facebook.png")} alt="" height={32} />
+                </i>
+                <i>
+                  <img src={require("../../assets/ico/twitter.png")} alt="" />
+                </i>
+                <i>
+                  <img src={require("../../assets/ico/instagram.png")} alt="" height={32} />
+                </i>
               </div>
             </div>
           </div>
