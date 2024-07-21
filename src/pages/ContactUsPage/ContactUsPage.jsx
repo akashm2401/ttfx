39ba312e38bf1c44d4d9a89015aecf32f009d3f0
Akash More <akashkmore2002@gmail.com>
2024-07-22 02:00:13 +0530

    contact us page done

--- src/pages/ContactUsPage/ContactUsPage.jsx
+++ src/pages/ContactUsPage/ContactUsPage.jsx
@@ -1,8 +1,9 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import mapImage from "../../assets/img/map-loc.png";
 import "./ContactUsPage.scss";
 import { TbMap2 } from "react-icons/tb";
-import ContectUsForm from "../../components/ContactForm/js/ContactUsForm";
+import ContactUsForm from "../../components/ContactForm/js/ContactUsForm";
+import SpinnerComponent from '../../components/SpinnerComponent/SpinnerComponent';
 import { BiSupport } from "react-icons/bi";
 
 export default function ContactUsPage() {
@@ -10,72 +11,71 @@ export default function ContactUsPage() {
     window.scrollTo(0, 0);
   }, []);
 
+
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch('/data/contactUsPageData.json')
+      .then((response) => response.json())
+      .then((data) => setData(data))
+      .catch((error) => console.error('Error fetching data:', error));
+  }, []);
+
+  if (!data) {
+    return <div><SpinnerComponent/></div>;
+  }
+
   return (
     <div className="contact-comp">
       <div className="container-fluid main-container">
-        <div className="row" style={{ marginTop: "50px" }}>
+        <div className="row" style={{ marginTop: "25px" }}>
           <div className="col-12 col-md-4" style={{ margin: "auto" }}>
             <div className="col-12 col-md-12">
               <div className="col-12 col-md-12 map-logo">
                 <TbMap2 size={36} color="#67a1d4" />
               </div>
-
-              <div className="col-md-12 map-logo-name">Malaysia</div>
+              <div className="col-md-12 map-logo-name"><strong>{data.country}</strong></div>
             </div>
 
             <div className="row">
-              <div className="col-12  col-md-12">
+              <div className="col-12 col-md-12">
                 <p>
-                  Office Suite 1652 Level16(A), Main Office Tower <br />
-                  Financial Park, Complex Labuan <br />
-                  Jalan Merdeka 87000, Labuan F.T
+                  {data.address.map((line, index) => (
+                    <React.Fragment key={index}>
+                      {line} <br />
+                    </React.Fragment>
+                  ))}
                 </p>
               </div>
-              {/* <div className="col-sm">
-                <hr className="divider"></hr>
-              </div> */}
 
-              <div className="col-12 ">
+              <div className="col-12">
                 <h4 className="heading-add">Enquiries Only</h4>
                 <div className="row py-1">
                   <div className="heading-label">
-                    Email &nbsp;
-                    {/* <dt>Email</dt> */}
-                    {/* <div className="col-7  "> */}
-                    <a href="">
-                      <strong>support@ttprofx.com</strong>
-                    </a> <br/>
-                    <a href="" className='ms-5 ps-1'>
-                      <strong>devansh@ttprofx.com</strong>
-                    </a>
-                    {/* </div> */}
+                    Email &nbsp; <br/>
+                    {data.emails.map((email, index) => (
+                      <a className="ms-4" href={`mailto:${email}`} key={index}>
+                        <strong>{email}</strong> <br />
+                      </a>
+                    ))}
                   </div>
                 </div>
 
                 <div className="row">
                   <div className="heading-label">
-                    Phone &nbsp;
-                    {/* <div className="col-7  "> */}
-                    <a href=""></a>
-                    <a href="">
-                      <strong>+60196508002</strong>
+                    Phone &nbsp; <br/>
+                    <a className="ms-4" href={`tel:${data.phone}`}>
+                      <strong>{data.phone}</strong>
                     </a>
-                    {/* </div> */}
                   </div>
                 </div>
                 <hr className="v2-divider v2-mav20"></hr>
               </div>
 
-              <div className="col-12 ">
-                <h4></h4>
+              <div className="col-12">
                 <div className="v2-mab30" data-qa="supportcentersection">
                   <div className="rte--output">
-                    <small>
-                      All enquiries should be addressed via the above channels.
-                      For meetings at our office, please contact us via the
-                      relevant channels above in advance to schedule an
-                      appointment.&nbsp;{" "}
-                    </small>
+                    <small>{data.note}</small>
                   </div>
                 </div>
                 <hr className="v2-divider v2-mav20"></hr>
@@ -84,7 +84,6 @@ export default function ContactUsPage() {
           </div>
 
           <div className="col-12 col-md-5 map-image-className">
-            {/* <h2>Our Location</h2> */}
             <div
               className="d-flex justify-content-center"
               style={{ position: "relative" }}
@@ -101,7 +100,7 @@ export default function ContactUsPage() {
                 }}
               />
               <a
-                href="https://maps.app.goo.gl/xcLfKw3jcDVJAc1e8"
+                href={data.mapLink}
                 target="_blank"
                 rel="noopener noreferrer"
                 style={{
@@ -119,42 +118,8 @@ export default function ContactUsPage() {
         </div>
       </div>
       <div className="card-form">
-        {/* <div className="container queries-card">
-          <div className="row">
-            <div className="queries-card-heding">
-              <h2>Other Inquiries</h2>
-            </div>
-            <div className="cards row">
-              <div className="col-12 col-md-6 m-0 p-0 d-flex justify-content-center card-main">
-                <div className="card">
-                  <BiSupport size={64} />
-                  <h5 className="card-title">Complaints</h5>
-                  <p className="card-text">
-                    For questions about sales, please contact our sales team.
-                  </p>
-                  <a className="underline-link" href="#">
-                    File a complaint
-                  </a>
-                </div>
-              </div>
-              <div className="col-12 col-md-6 m-0 p-0 d-flex justify-content-center card-main">
-                <div className="card">
-                  <BiSupport size={64} />
-                  <h5 className="card-title">Support</h5>
-                  <p className="card-text">
-                    If you need help, our support team is here for you.
-                  </p>
-                  <a className="underline-link" href="#">
-                    Visit the support centre
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div> */}
-
         <div className="row m-0 p-0">
-          <ContectUsForm />
+          <ContactUsForm />
         </div>
       </div>
     </div>
