b066bac3e1af5febcf6e6343f324c4e873b97162
Akash More <akashkmore2002@gmail.com>
2024-07-22 01:24:03 +0530

    Solution Page done.

--- src/pages/SolutionsPage/SolutionsPage.jsx
+++ src/pages/SolutionsPage/SolutionsPage.jsx
@@ -1,68 +1,38 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./SolutionsPage.scss";
+import SpinnerComponent from '../../components/SpinnerComponent/SpinnerComponent';
+import starIcon from '../../assets/ico/star.png';
+import penIcon from '../../assets/ico/pen.png';
+import iphoneIcon from '../../assets/ico/iphone.png';
 
 export default function SolutionsPage() {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
-  const products = [
-    {
-      id: 101,
-      name: "Clobber Demo",
-      info: {
-        description:
-          "To get your 48-hour credentials, simply enter 'guest' as the username and press Enter.",
-        linkTitle: "Demo",
-        link: "https://demofix.ttprofx.com:2228/login/index.html",
-      },
-    },
-    {
-      id: 102,
-      name: "Clobber Live",
-      info: {
-        description: "Acces the live clobber",
-        linkTitle: "Live here!",
-        link: "https://fix.ttprofx.com:2218/login/index.html",
-      },
-    },
-    {
-      id: 103,
-      name: "MT4 PC Client Termina",
-      info: {
-        description: "Description",
-        linkTitle: "Download here!",
-        link: "https://ttprofx.com/truetradepro4setup.exe",
-      },
-    },
-    {
-      id: 104,
-      name: "MT4 iOS Terminal",
-      info: {
-        description: "Description",
-        linkTitle: "Download here!",
-        link: "https://download.mql5.com/cdn/mobile/mt4/ios?server=TrueTradePro-Demo,TrueTradePro-Live",
-      },
-    },
-    {
-      id: 105,
-      name: "MT4 MacOS Terminal",
-      info: {
-        description: "Description ",
-        linkTitle: "Download here!",
-        link: "https://download.mql5.com/cdn/web/metaquotes.software.corp/mt4/MetaTrader4.dmg",
-      },
-    },
-    {
-      id: 106,
-      name: "MT4 Android Terminal",
-      info: {
-        description: "Description",
-        linkTitle: "Download here!",
-        link: "https://download.mql5.com/cdn/mobile/mt4/android?server=TrueTradePro-Demo,TrueTradePro-Live",
-      },
-    },
-  ];
+  const icons = {
+    star: starIcon,
+    pen: penIcon,
+    iphone: iphoneIcon
+  };
+
+  const [solutionData, setSolutionData] = useState(null);
+
+  useEffect(() => {
+    fetch("/data/solutionPAgeData.json")
+      .then(response => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok " + response.statusText);
+        }
+        return response.json();
+      })
+      .then(data => setSolutionData(data))
+      .catch(error => console.error("Error fetching features data:", error));
+  }, []);
+
+  if (!solutionData) {
+    return <div><SpinnerComponent /></div>;
+  }
 
   return (
     <div className="product">
@@ -71,7 +41,7 @@ export default function SolutionsPage() {
         <p className="text-center mb-2">Please select your download</p>
         <div className="container">
           <div className="row">
-            {products.map((product, index) => (
+            {solutionData?.products.map((product, index) => (
               <div className="cards col-sm-12 col-md-6 col-lg-6" key={index}>
                 <div className="card-main">
                   <div className="card">
@@ -91,215 +61,77 @@ export default function SolutionsPage() {
       <hr />
 
       <div className="product-container-2 container my-2">
-        <div className="row m-0 p-0">
-          <div className="col-12 col-sm-12 col-md-12 col-lg-7 pb-3">
-            <div className="product-info-box">
-              <h4>
-                <i className="me-3">
-                  <img
-                    src={require("../../assets/ico/star.png")}
-                    alt="star img"
-                    style={{ height: "30px" }}
-                  />
-                </i>
-                Mt4
-              </h4>
-              <div className="p-heading-line ms-5 my-2">
-                Download the True Trade Pro MetaTrader Terminal
-              </div>
-              <p className="text-justify">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Et
-                quibusdam deleniti numquam non. Tempora commodi a mollitia
-                reiciendis tempore incidunt cumque atque autem totam ad labore
-                eligendi similique in libero molestiae deserunt, voluptas hic?
-                Vel aliquid voluptates nesciunt ratione sint? Accusamus,
-                doloremque inventore. Exercitationem tenetur suscipit doloribus
-                enim, sit nostrum?
-              </p>
-            </div>
-          </div>
-
-          <div className="col-12 col-sm-12 col-md-12 col-lg-5 px-4 pb-3">
-            <div className=" product-download-box">
-              <div
-                className="row py-2 justify-content-between"
-                style={{ height: "100%" }}
-              >
-                <div className="col-12 col-sm-6  mb-3">
-                  <div className="product-download-box-name">
-                    MT4 PC Client Terminal
-                  </div>
-                  <div className="product-download-box-link">
-                    <a href="https://ttprofx.com/truetradepro4setup.exe" className="btn btn-sm btn-outline-primary">
-                      Download
-                    </a>
-                  </div>
-                </div>
-
-                <div className="col-12 col-sm-6  mb-3">
-                  <div className="product-download-box-name">
-                    MT4 iOS Terminal
-                  </div>
-                  <div className="product-download-box-link">
-                    <a href='https://download.mql5.com/cdn/mobile/mt4/ios?server=TrueTradePro-Demo,TrueTradePro-Live' className="btn btn-sm btn-outline-primary">
-                      Download
-                    </a>
-                  </div>
-                </div>
-
-                <div className="col-12 col-sm-6  mb-3">
-                  <div className="product-download-box-name">
-                    MT4 MacOS Terminal
-                  </div>
-                  <div className="product-download-box-link">
-                    <a href="https://download.mql5.com/cdn/web/metaquotes.software.corp/mt4/MetaTrader4.dmg" className="btn btn-sm btn-outline-primary">
-                      Download
-                    </a>
-                  </div>
-                </div>
-
-                <div className="col-12 col-sm-6  mb-3">
-                  <div className="product-download-box-name">
-                    MT4 Android Terminal
-                  </div>
-                  <div className="product-download-box-link">
-                    <a href='https://download.mql5.com/cdn/mobile/mt4/android?server=TrueTradePro-Demo,TrueTradePro-Live' className="btn btn-sm btn-outline-primary">
-                      Download
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <hr />
-
-          <div className="col-12 col-sm-12 col-md-12 col-lg-7 pb-3">
-            <div className="product-info-box">
-              <h4>
-                <i className="me-3">
-                  <img
-                    src={require("../../assets/ico/pen.png")}
-                    alt="star img"
-                    style={{ height: "30px" }}
-                  />
-                </i>
-                Custom
-              </h4>
-              <div className="p-heading-line ms-5 my-2">
-                Download the True Trade Pro MetaTrader Terminal
-              </div>
-              <p className="text-justify">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Et
-                quibusdam deleniti numquam non. Tempora commodi a mollitia
-                reiciendis tempore incidunt cumque atque autem totam ad labore
-                eligendi similique in libero molestiae deserunt, voluptas hic?
-                Vel aliquid voluptates nesciunt ratione sint? Accusamus,
-                doloremque inventore. Exercitationem tenetur suscipit doloribus
-                enim, sit nostrum?
-              </p>
-            </div>
-          </div>
-
-          <div className="col-12 col-sm-12 col-md-12 col-lg-5 px-4 pb-3">
-            <div className=" product-download-box">
-              <div
-                className="row py-2 justify-content-between"
-                style={{ height: "100%" }}
-              >
-                <div className="col-12 col-sm-6 mb-3">
-                  <div className="product-download-box-name">
+      <div className="row m-0 p-0">
+        {solutionData?.productsInfo.map((product, index) => (
+          <React.Fragment key={index}>
+            <div className="col-12 col-sm-12 col-md-12 col-lg-7 pb-3">
+              <div className="product-info-box">
+                <h4>
+                  <i className="me-3">
                     <img
-                      src={require("../../assets/img/metatrader-4-logo.png")}
-                      alt="metatrader-logo"
-                      style={{ height: "60px" }}
+                      src={icons[product.icon.split('.')[0]]}
+                      alt={`${product.icon.split('.')[0]} img`}
+                      style={{ height: "30px" }}
                     />
-                  </div>
-                  <div className="product-download-box-link">
-                    {/* <button className="btn btn-sm btn-outline-primary">Web Trader</button> */}
-                  </div>
+                  </i>
+                  {product.title}
+                </h4>
+                <div className="p-heading-line ms-5 my-2">
+                  {product.heading}
                 </div>
-
-                <div className="col-12 col-sm-6 mb-3">
-                  <div className="product-download-box-name">
-                    <button
-                      className="btn btn-sm btn-outline-primary"
-                      style={{ fontSize: "1rem" }}
-                    >
-                      Web Trader
-                    </button>
-                  </div>
-                  <div className="product-download-box-link">
-                    {/* <button className="btn btn-sm btn-outline-primary">Web Trader</button> */}
-                  </div>
+                <p className="text-justify">
+                  {product.description}
+                </p>
+              </div>
+            </div>
+            <div className="col-12 col-sm-12 col-md-12 col-lg-5 px-4 pb-3">
+              <div className="product-download-box">
+                <div
+                  className="row py-2 justify-content-between"
+                  style={{ height: "100%" }}
+                >
+                  {product.downloads.map((download, idx) => (
+                    <div className="col-12 col-sm-6 mb-3" key={idx}>
+                      <div className="product-download-box-name">
+                        {download.name}
+                      </div>
+                      <div className="product-download-box-link">
+                        {download.link ? (
+                          <a
+                            href={download.link}
+                            className="btn btn-sm btn-outline-primary"
+                          >
+                            Download
+                          </a>
+                        ) : (
+                            <div>
+                              <div className="mb-3"> 
+                                <button className="btn btn-sm btn-outline-primary">
+                                  Web Trader
+                                </button>
+                              </div>
+                              <div>
+                                <img
+                                  src={require("../../assets/img/metatrader-4-logo.png")}
+                                  alt="metatrader-logo"
+                                  style={{ height: "60px" }}
+                                />
+                              </div>
+                            </div>
+                        )}
+                      </div>
+                    </div>
+                  ))}
                 </div>
               </div>
             </div>
-          </div>
-
-          <hr />
-
-          <div className="col-12 col-sm-12 col-md-12 col-lg-7 pb-3">
-            <div className="product-info-box">
-              <h4>
-                <i className="me-3">
-                  <img
-                    src={require("../../assets/ico/iphone.png")}
-                    alt="star img"
-                    style={{ height: "30px" }}
-                  />
-                </i>
-                Documents
-              </h4>
-              <div className="p-heading-line ms-5 my-2">
-                Please see Documents for Download
-              </div>
-              <p className="text-justify">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Et
-                quibusdam deleniti numquam non. Tempora commodi a mollitia
-                reiciendis tempore incidunt cumque atque autem totam ad labore
-                eligendi similique in libero molestiae deserunt, voluptas hic?
-                Vel aliquid voluptates nesciunt ratione sint? Accusamus,
-                doloremque inventore. Exercitationem tenetur suscipit doloribus
-                enim, sit nostrum?
-              </p>
-            </div>
-          </div>
-
-          <div className="col-12 col-sm-12 col-md-12 col-lg-5 px-4 pb-3">
-            <div className=" product-download-box">
-              <div
-                className="row py-2 justify-content-between"
-                style={{ height: "100%" }}
-              >
-                <div className="col-12 col-sm-6 mb-3">
-                  <div className="product-download-box-name">
-                    Client Application
-                  </div>
-                  <div className="product-download-box-link">
-                    <a href="https://ttprofx.com/Client%20Application%20%20CCDD%20Form-%20TTPROFX%20V1.0%20April%202023.pdf" className="btn btn-sm btn-outline-primary">
-                      Download
-                    </a>
-                  </div>
-                </div>
-
-                <div className="col-12 col-sm-6 mb-3">
-                  <div className="product-download-box-name">
-                    Customer Compliant
-                  </div>
-                  <div className="product-download-box-link">
-                    <a href='https://ttprofx.com/Complaint%20Form%201.1.pdf' className="btn btn-sm btn-outline-primary">
-                      Download
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <hr />
-        </div>
+            <hr />
+          </React.Fragment>
+        ))}
       </div>
+    </div>
+
+
     </div>
   );
 }
